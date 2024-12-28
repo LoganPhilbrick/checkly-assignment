cdd141b9c59dcaf424b9eb3025c4f0311a8b1e61
LoganPhilbrick <loganphilbrick99@gmail.com>
2024-12-28 18:18:54 -0500

    dynamic api check for users stored in json and auth token stored in .env

---
+++ __checks__/user-api.check.ts
@@ -0,0 +1,21 @@
+import { ApiCheck, AssertionBuilder } from "checkly/constructs";
+import data from "../users.json";
+import "dotenv/config";
+
+const { authToken } = process.env;
+
+const users = data.users;
+
+for (const user of users) {
+  new ApiCheck(`User_${user.id}_check`, {
+    name: `${user.name} Check`,
+    request: {
+      url: `${user.profile_url}`,
+      method: "GET",
+      followRedirects: true,
+      skipSSL: false,
+      headers: [{ key: "Authorization", value: `Bearer ${authToken}` }],
+      assertions: [AssertionBuilder.statusCode().equals(200)],
+    },
+  });
+}
